471371af9aecbbb592537e9eaffc17912fb3242f
SK <skustov@lnova.ru>
2023-04-12 13:16:10 +0300

    C# Homework lesson2

---
+++ C#/homework/lesson2/task3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Программа, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.");
+
+int num;
+
+while (true)
+{
+    Console.Write("Введите цифру от 1 до 7: ");
+    string text = Console.ReadLine();
+    if (int.TryParse(text, out int number) && (number >=1) && (number <=7))
+    {
+        num = number;
+        if(num <= 5) {
+            Console.WriteLine($"{num} не является выходным");
+        }
+        else {
+            Console.WriteLine($"{num} является выходным");
+        }    
+    break;    
+    }
+    
+    Console.WriteLine("Не удалось распознать цифру.");
+}
+
