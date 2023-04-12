34213068cfc5b7e9637633bd06cfabc83119a351
SK <skustov@lnova.ru>
2023-04-12 13:17:54 +0300

    C# Lesson2 homework

---
+++ C#/homework/lesson2/task1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.");
+
+int num;
+int second_number;
+
+while (true)
+{
+    Console.Write("Введите трехзначное число: ");
+    string text = Console.ReadLine();
+    if (int.TryParse(text, out int number) && (number / 100 < 10 && number / 100 >= 1))
+    {
+            num = number;    
+        break;    
+    }
+    
+    Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+}
+
+
+second_number = (num / 10) % 10;
+
+System.Console.WriteLine($"Вторая цифра трехзначного числа {num} - {second_number}");
---
+++ C#/homework/lesson2/task2/Program.cs
@@ -0,0 +1,51 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.");
+
+
+int ThirdDigit(int number)
+        {
+            int result = -1;
+            if (number >= 100)
+            {
+                while (number > 999)
+                {
+                    number = number / 10;
+                }
+                result = number % 10;
+            }
+            return result; 
+        }
+
+int num;
+int digit;
+
+while (true)
+{
+    Console.Write("Введите число: ");
+    string text = Console.ReadLine();
+    if (int.TryParse(text, out int number))
+    {
+            num = number;    
+        break;    
+    }
+    
+    Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+}
+
+
+
+digit = ThirdDigit(num);
+
+if(digit < 0) {
+    System.Console.WriteLine($"у числа {num} нет третьей цифры");
+}
+
+else {
+    System.Console.WriteLine($"у числа {num} третья цифра {digit}");
+}
+
+
